c57beac90a0df946be3f6333d54c8c473517150a
xiaozhuai <798047000@qq.com>
2023-03-31 16:55:34 +0800

    Support 231.*

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "io.github.xiaozhuai"
-version = "1.1.1"
+version = "1.1.2"
 
 repositories {
     mavenCentral()
@@ -25,7 +25,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("212")
-        untilBuild.set("223.*")
+        untilBuild.set("231.*")
     }
 
     signPlugin {
--- src/main/java/io/github/xiaozhuai/jetbrains/qoi/QOIMetadataRegistrar.java
+++ src/main/java/io/github/xiaozhuai/jetbrains/qoi/QOIMetadataRegistrar.java
@@ -1,16 +1,11 @@
 package io.github.xiaozhuai.jetbrains.qoi;
 
-import com.intellij.openapi.application.PreloadingActivity;
-import com.intellij.openapi.progress.ProgressIndicator;
-import org.jetbrains.annotations.NotNull;
-
 import javax.imageio.spi.IIORegistry;
 import javax.imageio.spi.ImageReaderSpi;
 import javax.imageio.spi.ImageWriterSpi;
 
-public class QOIPreloadingActivity extends PreloadingActivity {
-    @Override
-    public void preload(@NotNull ProgressIndicator indicator) {
+public class QOIMetadataRegistrar {
+    public QOIMetadataRegistrar() {
         ensureQoiRegistered();
     }
 
