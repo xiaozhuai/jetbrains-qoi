8aef5edfe8022e6ffbc8b3c8a61bae1a29891213
xiaozhuai <798047000@qq.com>
2025-08-11 22:00:28 +0800

    Use 2.x gradle intellij plugin & Bump to 1.3.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,35 @@
 plugins {
     id("java")
-    id("org.jetbrains.intellij") version "1.17.3"
+    id("org.jetbrains.intellij.platform") version "2.7.1"
 }
 
 group = "io.github.xiaozhuai"
-version = "1.2.4"
+version = "1.3.0"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
-intellij {
-    version.set("2021.2")
-    type.set("IU")
+dependencies {
+    // !!! Do not forget to change QOIPluginConstants.QOI_VERSION when upgrade qoi-java !!!
+    implementation("me.saharnooby:qoi-java:1.2.1")
+    intellijPlatform {
+        create("IU", "2025.2")
+//        create("CL", "2025.2")
+        plugin("PsiViewer:252.23892.248")
+        plugin("com.cursiveclojure.cursive:2025.2-252")
+    }
+}
 
-    plugins.set(listOf("com.intellij.platform.images"))
+intellijPlatform {
+    pluginConfiguration {
+        ideaVersion {
+            sinceBuild = "212"
+        }
+    }
 }
 
 tasks {
@@ -27,11 +42,6 @@ tasks {
         enabled = false
     }
 
-    patchPluginXml {
-        sinceBuild.set("212")
-        untilBuild.set("252.*")
-    }
-
     signPlugin {
         certificateChain.set(System.getenv("INTELLIJ_CERTIFICATE_CHAIN"))
         privateKey.set(System.getenv("INTELLIJ_PRIVATE_KEY"))
@@ -41,9 +51,4 @@ tasks {
     publishPlugin {
         token.set(System.getenv("INTELLIJ_PUBLISH_TOKEN"))
     }
-}
-
-dependencies {
-    // !!! Do not forget to change QOIPluginConstants.QOI_VERSION when upgrade qoi-java !!!
-    implementation("me.saharnooby:qoi-java:1.2.1")
 }
