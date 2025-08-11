5d572171b60169bf0c9e4b1e7cdd4dcef3f37284
xiaozhuai <798047000@qq.com>
2025-08-11 20:48:54 +0800

    Support 252.* & Bump to 1.2.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 }
 
 group = "io.github.xiaozhuai"
-version = "1.2.3"
+version = "1.2.4"
 
 repositories {
     mavenCentral()
@@ -29,7 +29,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("212")
-        untilBuild.set("251.*")
+        untilBuild.set("252.*")
     }
 
     signPlugin {
